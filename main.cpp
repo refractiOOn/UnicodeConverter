675a47fb539c093aa64081ecae83e3186a109458
refractiOOn <etozhesotnikow@gmail.com>
2022-12-17 16:00:55 +0200

    convert function added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <string_view>
+#include <windows.h>
+
+std::wstring ConvertToUTF16(std::string_view source)
+{
+	int requiredSize = MultiByteToWideChar(CP_UTF8, 0, &source[0], -1, NULL, 0);
+	std::wstring result(requiredSize, '\0');
+	MultiByteToWideChar(CP_UTF8, 0, &source[0], -1, &result[0], requiredSize);
+	return result;
+}
 
 int main()
 {
+	std::string str = "\xE7\xB9\x81\xE4\xBD\x93\xE5\xAD\x97";
+	std::wstring result = ConvertToUTF16(str);
 
+	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+	WriteConsole(h, &result[0], result.length(), 0, 0);
 }
